cc3dfa13c4c8f30a61174a71f9c3ffea5eef0578
CHAPELLE Quentin <chapelle.quentin@gmail.com>
2018-12-12 11:30:53 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/f2-2018/precache-manifest.d8de0f258ed063eaaa53a3d1727c6b2d.js"
+  "/f2-2018/precache-manifest.b46f0bfd88fc8214e4d01bcbe230eb0e.js"
 );
 
 workbox.clientsClaim();
